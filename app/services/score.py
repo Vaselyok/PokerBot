32808b0f4fb3b79ed432113f011baf74ff12cbf3
kate-pant2011 <dik.pant2011@gmail.com>
2026-07-27 23:42:24 +0300

    keyboard 5

--- app/services/score.py
+++ app/services/score.py
@@ -51,7 +51,7 @@ async def close_table_and_update_elo(session, table_id, user_id):
         raise ApplicationException("Only organizer can close table", 400)
 
     table_players = await get_all_table_players_by_id(session, table_id)
-    assign_positions(table_players)
+    #assign_positions(table_players)
 
     if not table_players:
         raise ApplicationException("No players at table", 400)
@@ -59,10 +59,8 @@ async def close_table_and_update_elo(session, table_id, user_id):
     elo_results = []
     players = [tp.player for tp in table_players]
     total_players = len(players)
-    total_chips = sum(tp.chips for tp in table_players)
-    avg_chips = total_chips / total_players if total_players > 0 else 0
+    
     knockouts_map = defaultdict(list)
-    players_map = {p.id: p for p in players}
 
     for tp in table_players:
         if tp.eliminated_by_id:
@@ -76,16 +74,12 @@ async def close_table_and_update_elo(session, table_id, user_id):
     for tp in table_players:
         player = tp.player
 
-        opponents = [p for p in table_players if p.player_id != player.id]
+        #opponents = [p for p in table_players if p.player_id != player.id]
 
         elo_before = player.elo
 
-        elo_change = elo_delta(
-            tp,
-            opponents
-        )
-
-        elo_after = max(100, elo_before + elo_change)
+        elo_change = player.elo_change_per_match
+        elo_after = elo_before + elo_change
 ###################################
         player.elo = elo_after
         player.elo_change_per_match = 0
@@ -147,83 +141,83 @@ async def close_table_and_update_elo(session, table_id, user_id):
      )
 
 
-def elo_delta(table_player, opponents):
-    elo = table_player.player.elo
-    chips = table_player.chips
-    start = table_player.started_at
-    finish = table_player.finished_at
-    delta = 0
-    T = 5 * 60 * 60
-    K1, K2 = 100, 16 #48, 16
-    s_elo = 400 # не делаем вторую нормировку, этой достаточно
-    for opponent in opponents:
-        time = max(min(finish.timestamp(), opponent.finished_at.timestamp()) - max(start.timestamp(), opponent.started_at.timestamp()), 0)
-        p_ij = sigmoid((elo - opponent.player.elo)/s_elo)
-        part1_j  =  int(chips * opponent.chips != 0) * (sigmoid(np.log((chips + 50)/(opponent.chips + 50))) - p_ij) * time * K1 / T
+# def elo_delta(table_player, opponents):
+#     elo = table_player.player.elo
+#     chips = table_player.chips
+#     start = table_player.started_at
+#     finish = table_player.finished_at
+#     delta = 0
+#     T = 5 * 60 * 60
+#     K1, K2 = 100, 16 #48, 16
+#     s_elo = 400 # не делаем вторую нормировку, этой достаточно
+#     for opponent in opponents:
+#         time = max(min(finish.timestamp(), opponent.finished_at.timestamp()) - max(start.timestamp(), opponent.started_at.timestamp()), 0)
+#         p_ij = sigmoid((elo - opponent.player.elo)/s_elo)
+#         part1_j  =  int(chips * opponent.chips != 0) * (sigmoid(np.log((chips + 50)/(opponent.chips + 50))) - p_ij) * time * K1 / T
         
-        z_ij = 0.5
-        if finish.timestamp() > opponent.finished_at.timestamp() + 2:
-            z_ij = 1
-        elif finish.timestamp() < opponent.finished_at.timestamp() - 2:
-            z_ij = 0
+#         z_ij = 0.5
+#         if finish.timestamp() > opponent.finished_at.timestamp() + 2:
+#             z_ij = 1
+#         elif finish.timestamp() < opponent.finished_at.timestamp() - 2:
+#             z_ij = 0
         
-        part2_j  =  int(time > 1) * (z_ij - p_ij) * K2
+#         part2_j  =  int(time > 1) * (z_ij - p_ij) * K2
 
 
-        delta += part1_j + part2_j
-    delta = delta if delta > 0 else delta / 2.0
-    delta = min(60, max(delta, -60))
-    return delta
+#         delta += part1_j + part2_j
+#     delta = delta if delta > 0 else delta / 2.0
+#     delta = min(60, max(delta, -60))
+#     return delta
 
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
+# def sigmoid(x):
+#     return 1/(1+np.exp(-x))
 
-def bounty_bonus(player_id, knockouts_map, players_map):
-    victims = knockouts_map.get(player_id, [])
-    bonus = 0.0
-    for v_elo in victims:
-        raw = 5 + (v_elo - players_map[player_id].elo) / 100
-        bonus += max(2, raw)
-    return bonus
+# def bounty_bonus(player_id, knockouts_map, players_map):
+#     victims = knockouts_map.get(player_id, [])
+#     bonus = 0.0
+#     for v_elo in victims:
+#         raw = 5 + (v_elo - players_map[player_id].elo) / 100
+#         bonus += max(2, raw)
+#     return bonus
 
-def expected_score(player_elo, opponents):
-    if not opponents:
-        return 0.5
-    return sum(
-        1 / (1 + 10 ** ((opp - player_elo) / 400))
-        for opp in opponents
-    ) / len(opponents)
+# def expected_score(player_elo, opponents):
+#     if not opponents:
+#         return 0.5
+#     return sum(
+#         1 / (1 + 10 ** ((opp - player_elo) / 400))
+#         for opp in opponents
+#     ) / len(opponents)
 
-def actual_score(position, total):
-    if total <= 1:
-        return 1.0
+# def actual_score(position, total):
+#     if total <= 1:
+#         return 1.0
 
-    base_score = (total - position) / (total - 1)
-    return 0.1 + (base_score * 0.8)
+#     base_score = (total - position) / (total - 1)
+#     return 0.1 + (base_score * 0.8)
 
-def k_factor(games_played, elo):
-    if games_played < 10:
-        return 30
-    if elo < 1400:
-        return 15
-    return 10
+# def k_factor(games_played, elo):
+#     if games_played < 10:
+#         return 30
+#     if elo < 1400:
+#         return 15
+#     return 10
 
-def calculate_chips_bonus(player_chips: int, avg_chips: float) -> float:
-    if avg_chips == 0 or player_chips <= avg_chips:
-        return 0.0
+# def calculate_chips_bonus(player_chips: int, avg_chips: float) -> float:
+#     if avg_chips == 0 or player_chips <= avg_chips:
+#         return 0.0
 
-    bonus = ((player_chips - avg_chips) / avg_chips) * 10.0
-    return round(bonus, 2)
+#     bonus = ((player_chips - avg_chips) / avg_chips) * 10.0
+#     return round(bonus, 2)
 
 
-def assign_positions(table_players):
-    placed = [tp for tp in table_players if tp.position is not None]
-    active = [tp for tp in table_players if tp.position is None]
+# def assign_positions(table_players):
+#     placed = [tp for tp in table_players if tp.position is not None]
+#     active = [tp for tp in table_players if tp.position is None]
 
-    active.sort(key=lambda x: x.chips, reverse=True)
+#     active.sort(key=lambda x: x.chips, reverse=True)
 
-    current_position = 1
+#     current_position = 1
 
-    for tp in active:
-        tp.position = current_position
-        current_position += 1
+#     for tp in active:
+#         tp.position = current_position
+#         current_position += 1
