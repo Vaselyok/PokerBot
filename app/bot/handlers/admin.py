8f09523bf94968be07bda107af491441a5215b04
kate-pant2011 <dik.pant2011@gmail.com>
2026-07-29 21:48:08 +0300

    new logic 40

--- app/bot/handlers/admin.py
+++ app/bot/handlers/admin.py
@@ -700,7 +700,6 @@ async def cmd_shuffle(message: Message, bot: Bot, session: AsyncSession):
         tables = await get_active_tables(session, 1000, 0, game.id, sorting_rules)
         print(f"\nACTIVE TABLES {len(tables.items)}\n")
         players = await get_table_players_by_id(session, tables.items[0].id)
-        print(f"PLAYERS GOT {len(players.items) or "SHIT"}")
         data = await distribute_tables_for_shuffle(session, game.id, user.id, players)
         print("SHUFFLE DISTRIBUTED")
 
